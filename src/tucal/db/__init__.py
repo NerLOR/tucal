fb19bdd4a42a4b435bfab8979d99d866bf0826a5
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2021-11-17 15:55:03 +0100

    Moved db.py

--- src/tucal/db/__init__.py
+++ src/tucal/db/__init__.py
@@ -1,25 +1,37 @@
 
+from __future__ import annotations
 import typing
 import psycopg2
 
-DB_CONN: typing.Optional[psycopg2._psycopg.connection] = None
+DB_CONN: typing.Optional[Connection] = None
 
 DB_HOST = 'data.necronda.net'
 DB_NAME = 'tucal'
 DB_USER = 'necronda'
 DB_PASS = 'Password123'
+DB_TZ = 'Europe/Vienna'
 
 
-def connect() -> psycopg2._psycopg.connection:
+class Cursor(psycopg2._psycopg.cursor):
+    pass
+
+
+class Connection(psycopg2._psycopg.connection):
+    pass
+
+
+def connect() -> Connection:
     global DB_CONN
     DB_CONN = psycopg2.connect(f'host={DB_HOST} dbname={DB_NAME} user={DB_USER} password={DB_PASS}')
     return DB_CONN
 
 
-def cursor() -> psycopg2._psycopg.cursor:
+def cursor() -> Cursor:
     if DB_CONN is None:
         connect()
-    return DB_CONN.cursor()
+    cur = DB_CONN.cursor()
+    cur.execute(f"SET TIME ZONE '{DB_TZ}'")
+    return cur
 
 
 def commit() -> bool:
