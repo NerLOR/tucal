330a2478fae63735763335bc30aa77772db023c5
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2021-12-06 14:12:33 +0100

    Update minify-css.sh

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ build-www:
 	rm -rf dest/www
 	cp -pr www dest/www
 	sed -i 's|"\(/res/[^"]*\)"|"\1?v=$(shell date -u +%Y%m%d-%H%M%S)"|g' dest/www/.php/header.php
-	tools/minify-css.sh > dest/www/res/styles/min.css
+	tools/minify-css.sh
 	sed -i 's|/res/styles/styles.css|/res/styles/min.css|g' dest/www/.php/header.php
 
 	for locale in de de_AT de_DE en en_US en_GB bar; do \
--- tools/minify-css.sh
+++ tools/minify-css.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd 'dest/www/res/styles'
 files=$(echo 'styles.css'; grep -E '^@import "(.*)";' 'styles.css' | sed 's/@import "\|";//g')
-sed ':a;N;$!ba;s/[ \n]\{1,\}/ /g' $files | sed 's/ \?\([{}>;:,]\) \?/\1/g' | sed 's/@import[^;]*;\|^ \| $//g'
+wc -c $files
+sed ':a;N;$!ba;s/[ \n]\{1,\}/ /g' $files | sed 's/ \?\([{}>;:,]\) \?/\1/g' | sed 's/@import[^;]*;\|^ \| $//g' > min.css
+wc -c min.css
