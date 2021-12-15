187bad4f900d75d50745b43d7f13f8d8408198ac
Harald Moritz <harald.matthias.moritz@gmail.com>
2021-12-15 01:16:12 +0100

    Added eventHTU plugin.

--- src/tucal/plugins/__init__.py
+++ src/tucal/plugins/__init__.py
@@ -2,9 +2,11 @@
 import typing
 
 import tucal.plugins.c187B12
+import tucal.plugins.eventHTU
 
 
 def plugins() -> typing.List[typing.Tuple[str, typing.Type[tucal.Plugin]]]:
     return [
         ('187B12', c187B12.DWI),  # 187.B12 VU Denkweisen der Informatik
+        ('eventHTU', eventHTU.EVENTS),
     ]
