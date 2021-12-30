3c488d7ac16e817d37726fff3bec3137f1b347ee
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2021-12-30 15:21:33 +0100

    Fix login redirect

--- www/account/login.php
+++ www/account/login.php
@@ -53,8 +53,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (!$row['verified']) {
             redirect('/account/verify');
         } else {
-            redirect($_SESSION['redirect'] ?? '/');
-            unset($_SESSION['redirect']);
+            redirect($_SESSION['opts']['redirect'] ?? '/');
+            unset($_SESSION['opts']['redirect']);
         }
     } catch (Exception $e) {
         db_rollback();
