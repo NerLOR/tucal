68f886cdc3ac3a0a2887ac0773ccbc315c61daa9
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-01-27 17:06:18 +0100

    Update db.sh to parse ini files more correct

--- tools/db.sh
+++ tools/db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+regex="s/^[^=]\+=\\s*['\"]\?\(.*\)['\"]\?\\s*$/\\1/"
 db=$(grep '\[database\]' -A 10 tucal.ini)
-host=$(echo "$db" | grep "^host *=" | grep -o '[^= ]*$')
-port=$(echo "$db" | grep "^port *=" | grep -o '[^= ]*$')
-user=$(echo "$db" | grep "^user *=" | grep -o '[^= ]*$')
-name=$(echo "$db" | grep "^name *=" | grep -o '[^= ]*$')
-pwd=$(echo "$db" | grep "^password *=" | grep -o '[^= ]*$')
+host=$(echo "$db" | grep "^host *=" | sed "$regex")
+port=$(echo "$db" | grep "^port *=" | sed "$regex")
+user=$(echo "$db" | grep "^user *=" | sed "$regex")
+name=$(echo "$db" | grep "^name *=" | sed "$regex")
+pwd=$(echo "$db" | grep "^password *=" | sed "$regex")
+echo "$host"
 PGPASSWORD="$pwd" psql -h "$host" -p "$port" "$name" "$user" $@
