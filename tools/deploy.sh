610bace456c5d382170a14120d9247d486fef080
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-01-27 15:48:03 +0100

    Make deploy.sh DAU friendlier

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,15 +1,12 @@
 #/bin/bash
-
 host="$1"
 path="$2"
-
-if [ -z "$host"  ] || [ -z "$path" ]; then
+if [[ -z "$host"  || -z "$path" ]]; then
 	echo "deploy.sh: usage: deploy.sh <host> <path>" >&2
 	exit 1
 fi
-
 make build-www || exit
 
-ssh "$1" "cd $path; rm -rf *; rm -rf .php"
+ssh "$1" "cd \"$path\" && rm -rf * && rm -rf .php"
 scp -rpq dest/www/* "$1:$2"
 scp -rpq dest/www/.php "$1:$2"
