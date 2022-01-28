f9bae4c9ac6fb21da8f32ce741f2d6f52e5d8293
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-01-29 00:35:37 +0100

    Fix Inf.HS room code

--- typescript/calendar.ts
+++ typescript/calendar.ts
@@ -638,7 +638,7 @@ class WeekSchedule {
             html += `<div class="container"><div>${_('Room')}:</div><div class="room">`;
 
             const codes = room.getCodeFormat().replace(/ /g, '&nbsp;');
-            html += `<a href="https://toss.fsinf.at/?q=${room.roomCodes[0]}" target="_blank">` +
+            html += `<a href="https://toss.fsinf.at/?q=${room.ltRoomCode || room.roomCodes[0]}" target="_blank">` +
                 `<span class="room-name">${room.getNameLong()}</span> <span class="room-codes">(${codes})</span></a><br/>` +
                 `${room.getAddress().replace(/\n/g, '<br/>')}</div></div>`;
         }
