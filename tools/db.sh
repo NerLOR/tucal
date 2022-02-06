26354c5e5cf70b4710929f2e721726640870d68a
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-02-06 22:30:32 +0100

    FIX TOOLS/DB.SH ARGH

--- tools/db.sh
+++ tools/db.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-regex="s/^[^=]\+=\\s*['\"]\?\(.*\)['\"]\?\\s*$/\\1/"
+regex="s/^[^=]\+=\\s*\(['\"]\?\)\(.*\?\)\\1\\s*$/\\2/"
 db=$(grep '\[database\]' -A 10 tucal.ini)
 host=$(echo "$db" | grep "^host *=" | sed "$regex")
 port=$(echo "$db" | grep "^port *=" | sed "$regex")
 user=$(echo "$db" | grep "^user *=" | sed "$regex")
 name=$(echo "$db" | grep "^name *=" | sed "$regex")
 pwd=$(echo "$db" | grep "^password *=" | sed "$regex")
-echo "$host"
 PGPASSWORD="$pwd" psql -h "$host" -p "$port" "$name" "$user" $@
