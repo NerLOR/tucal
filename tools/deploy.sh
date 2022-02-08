7ee29ac39973967cc4d727fd9afbaf00721c927c
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-02-09 00:55:41 +0100

    Fix deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,8 +5,8 @@ if [[ -z "$host"  || -z "$path" ]]; then
 	echo "deploy.sh: usage: deploy.sh <host> <path>" >&2
 	exit 1
 fi
-make build-www || exit
 
-ssh "$1" "cd \"$path\" && rm -rf * && rm -rf .php"
-scp -rpq dest/www/* "$1:$2"
-scp -rpq dest/www/.php "$1:$2"
+make build-www || exit
+ssh "$host" "cd \"$path\" && rm -rf * && rm -rf .php"
+scp -rpq dest/www/* "$host:$path"
+scp -rpq dest/www/.php "$host:$path"
