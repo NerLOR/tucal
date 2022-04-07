9cf51b108c800629f95904e652efc94827ffbf07
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-04-07 19:27:30 +0200

    Update DAU display on start page

--- www/index.php
+++ www/index.php
@@ -1,13 +1,19 @@
 <?php
 
 global $STATUS;
+global $LOCALE;
 
 require ".php/session.php";
 require ".php/main.php";
 
 try {
     $stmt = db_exec("
-            SELECT users_v, users_v_sso, users_v_week, users_v_day
+            SELECT users_v, users_v_sso, users_v_week, (
+                SELECT round(avg(users_v_day))
+                FROM tucal.dau
+                WHERE date >= tucal.get_today() - INTERVAL '7' DAY AND
+                      date != tucal.get_today()
+                ), users_v_day
             FROM tucal.v_dau
             WHERE users_v IS NOT NULL
             LIMIT 1");
@@ -15,11 +21,14 @@ try {
     $userNum = $row[0];
     $credNum = $row[1];
     $weeklyNum = $row[2];
-    $todayNum = $row[3];
+    $avgDayNum = $row[3];
+    $todayNum = $row[4];
 } catch (Exception $e) {
     $STATUS = 500;
     $ERROR = $e->getMessage();
 }
+
+$formatter = new \NumberFormatter($LOCALE, \NumberFormatter::DECIMAL);
 
 require ".php/header.php";
 ?>
@@ -46,19 +55,23 @@ require ".php/header.php";
         <h1>Benutzerzahlen</h1>
         <div>
             <div>
-                <h2><?php echo $todayNum; ?></h2>
-                <h3>Heute aktiv</h3>
+                <h2><?php echo $formatter->format($todayNum); ?></h2>
+                <h3>Heute aktiv<br/>(ab 04:00)</h3>
             </div>
             <div>
-                <h2><?php echo $weeklyNum; ?></h2>
+                <h2><?php echo $formatter->format($avgDayNum); ?></h2>
+                <h3>Durchschnittlich pro Tag aktiv (7 Tage)</h3>
+            </div>
+            <div>
+                <h2><?php echo $formatter->format($weeklyNum); ?></h2>
                 <h3>In den letzten 7 Tagen aktiv</h3>
             </div>
             <div>
-                <h2><?php echo $credNum; ?></h2>
+                <h2><?php echo $formatter->format($credNum); ?></h2>
                 <h3>SSO-Zugangsdaten hinterlegt</h3>
             </div>
             <div>
-                <h2><?php echo $userNum; ?></h2>
+                <h2><?php echo $formatter->format($userNum); ?></h2>
                 <h3>Insgesamt verifiziert</h3>
             </div>
         </div>
